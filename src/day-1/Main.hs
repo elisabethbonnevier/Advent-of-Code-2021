ec6f33f5d2f7fe1e66f4094282a6f91c6f814123
Elisabeth Bonnevier <elisabeth@bonnevier.one>
2021-12-04 20:39:38 +0100

    Add solution to part 2

--- src/day-1/Main.hs
+++ src/day-1/Main.hs
@@ -13,3 +13,7 @@ main = do
             let diffs1 = zipWith (-) (drop 1 ints) ints
             putStr "Part 1: "
             putStrLn . show . length $ filter (> 0) diffs1
+            let threeWindowSums = zipWith3 (\a b c -> a + b + c) (drop 2 ints) (drop 1 ints) ints
+            let diffs2 = zipWith (-) (drop 1 threeWindowSums) threeWindowSums
+            putStr "Part 2: "
+            putStrLn . show . length $ filter (> 0) diffs2
