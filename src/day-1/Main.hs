ade7aac235e25aee22a4caf083e90d77883bb3ec
Elisabeth Bonnevier <elisabeth@bonnevier.one>
2021-12-04 19:41:41 +0100

    Add solution to part 1

--- src/day-1/Main.hs
+++ src/day-1/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import System.IO
+import System.IO.Utils
+
+import Text.Read
+
 main :: IO ()
-main = putStrLn "This works!"
+main = do
+    withFile "src/day-1/input.txt" ReadMode (\handle -> do
+        contents <- hGetLines handle
+        let maybeInts = traverse (readMaybe :: String -> Maybe Int) contents
+        case maybeInts of
+            Nothing -> putStrLn "There was an error in the input file."
+            Just ints -> do
+                let diffs = zipWith (-) (drop 1 ints) ints
+                putStrLn . show . length $ filter (> 0) diffs)
