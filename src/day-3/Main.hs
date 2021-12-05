5c7c86c07b23a6fbeee1104691e01a028e82f51f
Elisabeth Bonnevier <elisabeth@bonnevier.one>
2021-12-05 17:36:13 +0100

    Add solutions to day 3 part 2

--- src/day-3/Main.hs
+++ src/day-3/Main.hs
@@ -17,4 +17,22 @@ main = do
     let gammaBin = [if x > half then 1 else 0 | x <- colSums]
     let epsilonBin = map (1 -) gammaBin
     putStr "Part 1: "
-    putStrLn . show . product $ map (foldl' (\sum dig -> sum*2 + dig) 0) [gammaBin, epsilonBin]
+    print . product $ map (foldl' (\sum dig -> sum*2 + dig) 0) [gammaBin, epsilonBin]
+
+    -- Part 2
+    putStr "Part 2: "
+    print . product $ map (foldl' (\sum dig -> sum*2 + dig) 0) 
+                          [concat (filterByCommon (>) contentsList), -- oxygen generator rating
+                           concat (filterByCommon (<=) contentsList)] -- CO2 scrubber rating
+
+filterByCommon :: (Int -> Int -> Bool) -> [[Int]] -> [[Int]]
+filterByCommon f xs = filterByCommonAux 0 f xs where
+    filterByCommonAux :: Int -> (Int -> Int -> Bool) -> [[Int]] -> [[Int]]
+    filterByCommonAux n f [x] = [x]
+    filterByCommonAux n f xs =
+        let (zeros, ones) = foldr (\binNum (zeros, ones)
+                                    -> if binNum !! n == 0
+                                          then (binNum : zeros, ones)
+                                          else (zeros, binNum : ones))
+                                    ([], []) xs in
+        filterByCommonAux (n+1) f (if length zeros `f` length ones  then zeros else ones)
