218bb125b41aac76c6d9d4c85552af61b86d1faa
Elisabeth Bonnevier <elisabeth.bonnevier@uib.no>
2021-12-06 11:20:47 +0100

    Add solution to day 4 part 2

--- src/day-4/Main.hs
+++ src/day-4/Main.hs
@@ -1,51 +1,63 @@
 module Main where
 
-import Data.List (transpose, find)
+import Data.List (find, transpose)
 
 main :: IO ()
 main = do
-    -- Get contents
-    (numbers : boards) <- lines <$> readFile "src/day-4/input.txt"
-    let numbersList = map (read :: String -> Int) $ splitAtComma numbers
-    let boardsList = map (map (map (Unmarked . (read :: String -> Int)) . words))
-                         $ splitAtBoards boards
-    
-    -- Part 1
-    putStr "Part 1: "
-    let (number, board) = playBingoToWin numbersList boardsList
-    print $ number * sumUnmarked board
+  -- Get contents
+  (numbers : boards) <- lines <$> readFile "src/day-4/input.txt"
+  let numbersList = map (read :: String -> Int) $ splitAtComma numbers
 
-    -- Part 2
-    putStr "Part 2: "
-    let (numberd, board) = playBingoToLose numbersList boardsList
-    print $ number * sumUnmarked board
+  -- Part 1
+  putStr "Part 1: "
+  let unmarkedBoardsList =
+        map (map (map (Unmarked . (read :: String -> Int)) . words)) $
+          splitAtBoards (tail boards)
+  let (winningNumber, winningBoard) = playBingoToWin numbersList unmarkedBoardsList
+  print $ winningNumber * sumUnmarked winningBoard
+
+  -- Part 2
+  putStr "Part 2: "
+  let markedBoardsList =
+        map (map (map (Marked . (read :: String -> Int)) . words)) $
+          splitAtBoards (tail boards)
+  let (losingNumber, losingBoard) = playBingoToLose (reverse numbersList) markedBoardsList
+  print $ losingNumber * sumUnmarked losingBoard
 
 splitAtComma :: String -> [String]
 splitAtComma str =
-    case break (==',') str of
-        (a, ',':as) -> a : splitAtComma as
-        (a, "") -> [a]
+  case break (== ',') str of
+    (a, ',' : as) -> a : splitAtComma as
+    (a, "") -> [a]
 
 splitAtBoards :: [String] -> [[String]]
 splitAtBoards strs =
-    case break null strs of
-        (s, "":strs) -> s : splitAtBoards strs
-        (s, []) -> [s]
+  case break null strs of
+    (s, "" : strs) -> s : splitAtBoards strs
+    (s, []) -> [s]
 
 data BoardNumber = Unmarked Int | Marked Int
+  deriving (Show)
 
 type Board = [[BoardNumber]]
 
 markNumber :: Int -> [Board] -> [Board]
-markNumber num = map (map (map $ markUnmarked num)) where
-
+markNumber num = map (map (map $ markUnmarked num))
+  where
     markUnmarked :: Int -> BoardNumber -> BoardNumber
     markUnmarked m (Unmarked n) = if m == n then Marked n else Unmarked n
-    markUnmarked m (Marked n) = Marked num
+    markUnmarked m (Marked n) = Marked n
+
+unmarkNumber :: Int -> [Board] -> [Board]
+unmarkNumber num = map (map (map $ unmarkMarked num))
+  where
+    unmarkMarked :: Int -> BoardNumber -> BoardNumber
+    unmarkMarked m (Unmarked n) = Unmarked n
+    unmarkMarked m (Marked n) = if m == n then Unmarked n else Marked n
 
 wins :: Board -> Bool
-wins board = any markedLine board || any markedLine (transpose board) where
-
+wins board = any markedLine board || any markedLine (transpose board)
+  where
     markedLine :: [BoardNumber] -> Bool
     markedLine [] = True
     markedLine (Unmarked num : nums) = False
@@ -53,33 +65,21 @@ wins board = any markedLine board || any markedLine (transpose board) where
 
 playBingoToWin :: [Int] -> [Board] -> (Int, Board)
 playBingoToWin (number : numbers) boards =
-    let updatedBoards = markNumber number boards in
-    case find wins updatedBoards of
-         Nothing -> playBingoToWin numbers updatedBoards
-         Just board -> (number, board)
+  let updatedBoards = markNumber number boards
+   in case find wins updatedBoards of
+        Nothing -> playBingoToWin numbers updatedBoards
+        Just board -> (number, board)
 
 playBingoToLose :: [Int] -> [Board] -> (Int, Board)
--- playBingoToLose (number : numbers) boards
---     | length boards == 1 = 
---         let updatedBoards = markNumber number boards in
---         if any wins updatedBoards
---            then (number, head updatedBoards)
---            else playBingoToLose numbers boards
---     | otherwise = 
---         let updatedBoards = markNumber number boards in
---             playBingoToLose numbers (filter (not . wins) updatedBoards)
-playBingoToLose (number : numbers) [board] =
-    let [updatedBoard] = markNumber number [board] in
-    if wins updatedBoard
-       then (number, updatedBoard)
-       else playBingoToLose numbers [board]
 playBingoToLose (number : numbers) boards =
-    let updatedBoards = markNumber number boards in
-    playBingoToLose numbers (filter (not . wins) updatedBoards)
+  let updatedBoards = unmarkNumber number boards
+   in case find (not . wins) updatedBoards of
+        Nothing -> playBingoToLose numbers updatedBoards
+        Just board -> (number, head $ markNumber number [board])
 
 sumUnmarked :: Board -> Int
-sumUnmarked board = sum $ map sumUnmarkedLine board where
-
+sumUnmarked board = sum $ map sumUnmarkedLine board
+  where
     sumUnmarkedLine :: [BoardNumber] -> Int
     sumUnmarkedLine [] = 0
     sumUnmarkedLine (Unmarked num : nums) = num + sumUnmarkedLine nums
